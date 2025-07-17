d2e44d628498c32334f1c0bf283f1e80f8f4610a
Boyanz78 <yana.supriatna81@gmail.com>
2025-07-17 21:52:22 +0700

    add brandPrices.js

---
+++ brandPrices.js
@@ -0,0 +1,33 @@
+// brandPrices.js
+// File ini berisi data harga dasar untuk setiap merek bata ringan
+// beserta penyesuaian ongkos kirim spesifik berdasarkan lokasi.
+
+const baseBrandPrices = {
+    "SSI": { 
+        price: 390000, 
+        locationAdjustments: {} // Tidak ada penyesuaian khusus untuk SSI
+    },
+    "Platinum": { 
+        price: 380000, 
+        locationAdjustments: {
+            "Bandung Barat": {
+                "Cikalong Wetan": -5000 // Platinum lebih murah 5000 untuk Cikalong Wetan
+            },
+            "Bandung": {
+                "Cicalengka": 3000 // Platinum lebih mahal 3000 untuk Cicalengka
+            }
+        }
+    },
+    "Acon": { 
+        price: 375000, 
+        locationAdjustments: {} // Tidak ada penyesuaian khusus untuk Acon
+    },
+    "SBI": { 
+        price: 410000,
+        locationAdjustments: {
+            "Bandung Barat": {
+                "Cipeundeuy": 10000 // SBI lebih mahal 10000 untuk Cipeundeuy
+            }
+        }
+    }
+};
